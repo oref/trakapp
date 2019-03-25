078378df8ebae8a2a4fdf79931dbae1006543371
Harry Morgan <ferovax@gmail.com>
2019-03-25 21:13:52 +0000

    Added bujo

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,3 +4,4 @@ routes = Blueprint('routes', __name__)
 from .index import *
 from .login import *
 from .google_auth import *
+from .bujo import *
---
+++ routes/bujo.py
@@ -0,0 +1,13 @@
+from flask import render_template, redirect, url_for, flash, request, session, flash, g, Blueprint
+from routes import *
+
+@routes.route('/bujo')
+def bujo():
+    return render_template('bujo.html')
+
+def welcome_user():
+    if session['logged_in']:
+        welcome_status = 'Welcome {0}'.format(session['user'])
+    else:
+        welcome_status = 'You are not logged in'
+        return dict(welcome=welcome_status)
